8c5c5d069231c6e28992381cd2a59e3d29b578a6
David Larronda <david.larronda@gmail.com>
2021-08-16 13:04:43 -0300

    Primeiro commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  background(255); let turtle = makeTurtle(width/2, height/2);
+// -------------------------------------------------
+
+  
+  turtle.setColor('#8247bd')
+  turtle.setWeight(10)
+
+  turtle.forward(100)
+  
+  
+  
+// -------------------------------------------------  
+} function draw() {}
